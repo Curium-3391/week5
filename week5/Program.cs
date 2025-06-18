60acb16d5430845ca3e32ae3e781c678d46585bc
Curium-3391 <az95050731@gmail.com>
2025-06-18 14:23:19 +0800

    Add files via upload

---
+++ week5/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace week5
+{
+    internal class Program
+    {
+        public static string a;
+        static int add(int x, int y)
+        {
+            int value = 0;
+            for (int i = x; i < y; i++)
+            {
+                value += i;
+            }
+
+            return value;
+        }
+        static string add(string x, int y)
+        {
+            return x + y.ToString();
+        }
+        static void Main(string[] args)
+        {
+            string a = add("Hello", 11327015);
+            int b = add(60, 70);
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.Read();
+
+        }
+    }
+}
